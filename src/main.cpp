f245001916d762d04943c16b27ab441ec7e6a58b
Arbitrariest <etienneavandermerwe@gmail.com>
2026-06-07 23:01:19 +0200

    excitement

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,11 @@
 int main(){
 
     std::cout << "Check if build works";
+    std::cout << std::endl;
     std::cout << "Testing code review2";
+    std::cout << std::endl;
 
+    std::cout << "We're gonna do a fysiks!";
     return 0;
 
 }
